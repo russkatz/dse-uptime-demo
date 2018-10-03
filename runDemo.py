04dc2169bb9ab1a34a1135d1c14b536e19368a47
Your Name <you@example.com>
2018-10-03 15:38:59 +0000

    updating scripts

--- runDemo.py
+++ runDemo.py
@@ -2,6 +2,7 @@
 
 import sys
 from random import randint
+import time
 
 from dse.cluster import Cluster, ExecutionProfile, EXEC_PROFILE_DEFAULT
 from dse.auth import PlainTextAuthProvider
@@ -10,12 +11,12 @@ from dse import ConsistencyLevel
 
 #Configuration
 contactpoints = ['172.31.2.63', '172.31.9.65']
-localDC = "dc1"
+localDC = "AWS"
 CL = ConsistencyLevel.ONE
 #CL = ConsistencyLevel.ALL
 auth_provider = PlainTextAuthProvider (username='user1', password='password1')
 profile1 = ExecutionProfile( load_balancing_policy=DCAwareRoundRobinPolicy(local_dc=localDC, used_hosts_per_remote_dc=3),
-                            speculative_execution_policy=ConstantSpeculativeExecutionPolicy(.1, 20),
+                            speculative_execution_policy=ConstantSpeculativeExecutionPolicy(.05, 20),
                             consistency_level = CL
                             )
 
@@ -28,7 +29,8 @@ cluster = Cluster( contact_points=contactpoints,
 
 session = cluster.connect()
 
-session.execute (""" CREATE KEYSPACE IF NOT EXISTS stats WITH replication = {'class': 'NetworkTopologyStrategy', 'dc1': '3', 'dc2': '3'}  AND durable_writes = true """)
+#session.execute (""" CREATE KEYSPACE IF NOT EXISTS stats WITH replication = {'class': 'NetworkTopologyStrategy', 'dc1': '3', 'dc2': '3'}  AND durable_writes = true """)
+session.execute (""" CREATE KEYSPACE IF NOT EXISTS stats WITH replication = {'class': 'NetworkTopologyStrategy', 'AWS': '3', 'Google': '3'}  AND durable_writes = true """)
 session.execute (""" CREATE TABLE IF NOT EXISTS  stats.player_stats ( name text, game text, ts timeuuid, stat1 int, stat2 int, stat3 int, PRIMARY KEY ((name, game), ts)) """)
 
 c = 0
@@ -40,11 +42,16 @@ while 1:
    stat1 = randint(1,100)
    stat2 = randint(1,100)
    game = randint(1,100)
+
+   ts = int(time.time() * 1000)
    session.execute (""" INSERT INTO stats.player_stats (name, game, ts, stat1, stat2, stat3) VALUES (%s, %s, now(), %s, %s, %s) """, (str(name), str(game), int(stat0), int(stat1), int(stat2)))
    #session.execute_async (""" INSERT INTO stats.player_stats (name, game, ts, stat1, stat2, stat3) VALUES (%s, %s, now(), %s, %s, %s) """, (str(name), str(game), int(stat0), int(stat1), int(stat2)))
+   #sleep(0.01)
+   while ts + 30 > int(time.time() * 1000):
+    t1 = 0
    c = c + 1
    x = x + 1
-   if(x == 1000):
+   if(x == 100):
       print(""" Rows Written %s """ ) % c
       x = 0
 
