4f1a9abde9263db1e84c0e5c3493bb21781aad22
Your Name <you@example.com>
2018-10-08 16:26:43 +0000

    adding read and write demo scripts

---
+++ readDemo.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+import sys
+from random import randint
+import time
+
+from dse.cluster import Cluster, ExecutionProfile, EXEC_PROFILE_DEFAULT
+from dse.auth import PlainTextAuthProvider
+from dse.policies import DCAwareRoundRobinPolicy,TokenAwarePolicy, ConstantSpeculativeExecutionPolicy
+from dse import ConsistencyLevel
+
+#Configuration
+contactpoints = ['40.78.69.234', '104.42.194.135']
+localDC = "Azure"
+CL = ConsistencyLevel.ONE
+#CL = ConsistencyLevel.ALL
+auth_provider = PlainTextAuthProvider (username='user1', password='password1')
+profile1 = ExecutionProfile( load_balancing_policy=DCAwareRoundRobinPolicy(local_dc=localDC, used_hosts_per_remote_dc=3),
+                            speculative_execution_policy=ConstantSpeculativeExecutionPolicy(.05, 20),
+                            consistency_level = CL
+                            )
+
+print "Connecting to cluster"
+
+cluster = Cluster( contact_points=contactpoints,
+                   auth_provider=auth_provider,
+                   execution_profiles={EXEC_PROFILE_DEFAULT: profile1},
+                   )
+
+session = cluster.connect()
+
+used_dc = localDC
+coordinator = localDC
+c = 0
+x = 0
+while 1:
+
+   current = time.localtime()
+   bucket = str(current.tm_year) + str(current.tm_mon) + str(current.tm_mday) + str(current.tm_hour) + str(current.tm_min)
+
+   ts = int(time.time() * 1000)
+
+   query = """ select * from demo.table1 where bucket = '%s' limit 1 """ % (bucket)
+   results = session.execute (query)
+   for r in results:
+    d = r.d
+   while ts + 30 > int(time.time() * 1000):
+    t1 = 0
+   c = c + 1
+   x = x + 1
+   if(x == 10):
+    last_c = coordinator
+    future = session.execute_async (query, trace=True )
+    result = future.result()
+    try:
+     trace = future.get_query_trace()
+     coordinator =  trace.coordinator
+    except:
+     coordinator = last_c
+    for h in session.hosts:
+     if h.address == coordinator:
+      used_dc = h.datacenter
+      print(""" Rows Read %s (%s) - %s""" ) % (c, used_dc, d)
+    x = 0
+
+cluster.shutdown()
+sys.exit(0)
---
+++ writeDemo.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+import sys
+from random import randint
+import time
+
+from dse.cluster import Cluster, ExecutionProfile, EXEC_PROFILE_DEFAULT
+from dse.auth import PlainTextAuthProvider
+from dse.policies import DCAwareRoundRobinPolicy,TokenAwarePolicy, ConstantSpeculativeExecutionPolicy
+from dse import ConsistencyLevel
+
+#Configuration
+contactpoints = ['172.31.2.63', '172.31.9.65']
+localDC = "AWS"
+CL = ConsistencyLevel.ONE
+#CL = ConsistencyLevel.ALL
+auth_provider = PlainTextAuthProvider (username='user1', password='password1')
+profile1 = ExecutionProfile( load_balancing_policy=DCAwareRoundRobinPolicy(local_dc=localDC, used_hosts_per_remote_dc=3),
+                            speculative_execution_policy=ConstantSpeculativeExecutionPolicy(.05, 20),
+                            consistency_level = CL
+                            )
+
+print "Connecting to cluster"
+
+cluster = Cluster( contact_points=contactpoints,
+                   auth_provider=auth_provider,
+                   execution_profiles={EXEC_PROFILE_DEFAULT: profile1},
+                   )
+
+session = cluster.connect()
+
+session.execute (""" CREATE KEYSPACE IF NOT EXISTS demo WITH replication = {'class': 'NetworkTopologyStrategy', 'AWS': 3} """)
+session.execute (""" CREATE TABLE IF NOT EXISTS  demo.table1 (     bucket text,     ts timeuuid,     d text,     data1 int,     data2 int,     data3 int,     PRIMARY KEY (bucket, ts)) WITH CLUSTERING ORDER BY (ts desc) """)
+
+coordinator = localDC
+used_dc = localDC
+c = 0
+x = 0
+while 1:
+
+   data1 = randint(1,100)
+   data2 = randint(1,100)
+   data3 = randint(1,100)
+   current = time.localtime()
+   bucket = str(current.tm_year) + str(current.tm_mon) + str(current.tm_mday) + str(current.tm_hour) + str(current.tm_min)
+   d = time.strftime('%a, %d %b %Y %H:%M:%S', current)
+   query = """ INSERT INTO demo.table1 (bucket, ts, d, data1, data2, data3) VALUES ('%s', now(), '%s', %s, %s, %s) """ % (str(bucket), str(d), int(data1), int(data2), int(data3))
+   session.execute (query)
+   ts = int(time.time() * 1000)
+   while ts + 30 > int(time.time() * 1000):
+    t1 = 0
+   c = c + 1
+   x = x + 1
+   if(x == 10):
+    last_c = coordinator
+    future = session.execute_async (query, trace=True )
+    result = future.result()
+    try:
+     trace = future.get_query_trace()
+     coordinator =  trace.coordinator
+    except:
+     coordinator = last_c
+    for h in session.hosts:
+     if h.address == coordinator:
+      used_dc = h.datacenter
+    print(""" Rows Written %s (%s) - %s""" ) % (c, used_dc, d)
+    x = 0
+
+cluster.shutdown()
+sys.exit(0)
