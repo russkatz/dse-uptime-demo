277839b07a6f3ab13fa05ab08864847b0103cce8
Your Name <you@example.com>
2018-10-11 20:03:06 +0000

    updating decom to use LCM

--- decom.sh
+++ decom.sh
@@ -12,12 +12,15 @@ if [ -z "$1" ]; then
  exit 0
 fi
 
+echo "THIS WILL REMOVE THE ENTIRE $1 DC! PRESS ENTER TO CONTINE!"
+read waitforenter
+
 for i in `curl -s http://${lcm}:${lcmport}/${clustername}/nodes | jq  -rc '.[].node_ip + "^" + .[].dc' | grep -i "$1$" | awk -F^ '{print $1}'`; do
  echo "Running decommission and data reset on $i"
- echo ssh -oStrictHostKeyChecking=no -i $keyfile $username@$i "sudo nodetool decommission -f"
- echo ssh -oStrictHostKeyChecking=no -i $keyfile $username@$i "sudo pkill -9 -f dse.jar"
- echo ssh -oStrictHostKeyChecking=no -i $keyfile $username@$i "sudo rm -rf /var/lib/cassandra/commitlog"
- echo ssh -oStrictHostKeyChecking=no -i $keyfile $username@$i "sudo rm -rf /var/lib/cassandra/data"
- echo ssh -oStrictHostKeyChecking=no -i $keyfile $username@$i "sudo rm -rf /var/lib/cassandra/hints"
- echo ssh -oStrictHostKeyChecking=no -i $keyfile $username@$i "sudo rm -rf /var/lib/cassandra/saved_caches"
+ ssh -oStrictHostKeyChecking=no -i $keyfile $username@$i "sudo nodetool decommission -f"
+ ssh -oStrictHostKeyChecking=no -i $keyfile $username@$i "sudo pkill -9 -f dse.jar"
+ ssh -oStrictHostKeyChecking=no -i $keyfile $username@$i "sudo rm -rf /var/lib/cassandra/commitlog"
+ ssh -oStrictHostKeyChecking=no -i $keyfile $username@$i "sudo rm -rf /var/lib/cassandra/data"
+ ssh -oStrictHostKeyChecking=no -i $keyfile $username@$i "sudo rm -rf /var/lib/cassandra/hints"
+ ssh -oStrictHostKeyChecking=no -i $keyfile $username@$i "sudo rm -rf /var/lib/cassandra/saved_caches"
 done
