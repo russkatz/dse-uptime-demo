1d83f8f3715643a386c22a3f9d98f10a686c2d80
Beth Damato <bethadamato@gmail.com>
2019-01-10 21:41:20 -0500

    adjust look-feel on three sim pages

---
+++ ui/src/components/TotalLocalDcFail.js
@@ -0,0 +1,27 @@
+import React, {PureComponent} from 'react';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Stepper from './Stepper';
+import Dashboard from './Dashboard';
+import ReadWriteCard from './ReadWriteCard';
+
+export default class HomePage extends PureComponent {
+
+    render() {
+        return (
+            <Paper className="paper" elevation={1}>
+            <div className="container">
+            <Typography variant="h6" color="inherit" noWrap>
+                Total Local Data Center Failure
+            </Typography>
+            </div>
+            <Grid>
+                <Stepper />
+                <Dashboard />
+                <ReadWriteCard />
+            </Grid>
+            </Paper>
+        );
+    }
+}
