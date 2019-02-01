6d140a567356474f1b6a9b986f265b27dcdd77df
Your Name <you@example.com>
2019-02-01 16:02:37 +0000

    updated jq synatx

--- makeini.sh
+++ makeini.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 inifile=$1
-cluster=`curl -s http://localhost:8888/cluster-configs | jq -r 'keys | .[0]'`
+cluster=`curl -s http://localhost:8888/cluster-configs | jq -r 'keys[0]'`
 echo $cluster
 nodes=`curl -s http://localhost:8888/$cluster/nodes | jq -rc ".[].node_ip" | tr '\n' ',' | sed 's/\,$//g'`
 
