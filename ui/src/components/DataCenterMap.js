70761c25841ec6d6e28a4b3d626ea1e1a68abe9b
Beth Damato <bethadamato@gmail.com>
2019-03-27 21:44:20 -0400

    starting adjustments for capturing node.last_seen logic for dashboard

--- ui/src/components/DataCenterMap.js
+++ ui/src/components/DataCenterMap.js
@@ -18,7 +18,7 @@ const DataCenterMap = (props) => {
     return (
         <div className={classes.root}>
             <Paper style={{ backgroundColor: 'white', width: '90%', height: '420px', margin: '0 auto'}}>
-                <img style={{ width: '100%', height: '420px' }} src={require('../images/worldmap2.png')} />
+                {/* INSERT MAP HERE */}
             </Paper>
         </div>
     );
